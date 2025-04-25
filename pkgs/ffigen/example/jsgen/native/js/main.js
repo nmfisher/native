29e1db7ee2cadd6100d98c5145cd2df2287080f2
Nick Fisher <nick.fisher@avinium.com>
2025-04-25 08:18:21 +0800

    cleanup example JS loader

--- pkgs/ffigen/example/jsgen/native/js/main.js
+++ pkgs/ffigen/example/jsgen/native/js/main.js
@@ -3,11 +3,6 @@ import { compile } from './example.mjs';
 const moduleJs = read('example_lib.js')
 globalThis.eval(moduleJs);
 globalThis['module'] = await example();
-globalThis['printf'] = (v) => {
-    console.log(v);
-}
-console.log(Object.keys(globalThis['module']));
-console.log(typeof(globalThis['module']['HEAPU8']));
 
 const wasmBytes = readbuffer('example.wasm');
 
